1c841c3ec8a30138ec4f33d02e538bf47063d671
Eitz <richard@eitz.me>
2020-07-26 10:59:46 +0200

    Update examples
    Signed-off-by: Eitz <richard@eitz.me>

--- examples/thread.js
+++ examples/thread.js
@@ -1,24 +1,21 @@
-self.importScripts('pow.js');
+self.importScripts("pow.js");
 
 var ready = false;
 
-Module['onRuntimeInitialized'] = function() {
-    postMessage('ready');
-}
+Module["onRuntimeInitialized"] = function () {
+  postMessage("ready");
 
-onmessage = function(ev)
-{
-    var PoW = Module.cwrap("launchPoW", 'string', ['string']);
+  onmessage = function (ev) {
+    var PoW = (hash) => Module.ccall("getPowHeavy", "string", ["string"], hash);
     var hash = ev.data;
-	//let generate = Module.ccall("launchPoW", 'string', ['string'], hash);
-	let generate = PoW(hash);
-	
-	if (generate != "0000000000000000") {
-	    console.log(generate +" found");
-		postMessage(generate); // Worker return
-	}
-	else
-	{
-	    postMessage(false);
-	}
-}
+    //let generate = Module.ccall("getPoW", 'string', ['string'], hash);
+    let generate = PoW(hash);
+
+    if (generate != "0000000000000000") {
+      console.log(generate + " found");
+      postMessage(generate); // Worker return
+    } else {
+      postMessage(false);
+    }
+  };
+};
